34701e8309a2b658cbd745af11701442bf6c1927
plaknasehlok <sankalpgkolhe@gmail.com>
2019-11-20 10:26:00 +0530

    firsty commit

---
+++ demoSpringgit/src/main/java/com/example/demo/DemoSpringgitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringgitApplication.class, args);
+	}
+
+}
